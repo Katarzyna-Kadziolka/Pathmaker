fde19bd28318da11870271ce7fad8d16a19ce190
Katarzyna <kasiakadziolka@gmail.com>
2024-06-04 11:46:29 +0200

    i18n conf

--- web/src/main.ts
+++ web/src/main.ts
@@ -6,9 +6,20 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import en from "./locales/en.json"
+import {createI18n} from "vue-i18n";
+
+const i18n = createI18n({
+    locale: navigator.language,
+    fallbackLocale: "en",
+    messages: { en },
+    legacy: false
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
